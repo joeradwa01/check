54a3c6cf495eb0f3135a27902182711fbff00e54
joe <joeradwan01@gmail.com>
2023-03-19 17:20:31 +0300

    checkbox

---
+++ index.js
@@ -0,0 +1,39 @@
+
+// agree on terms check box
+function myFunction() {
+ 
+    var x = document.getElementById("myCheck").required;
+document.getElementById("demo").innerHTML = x;
+}
+
+const rmCheck = document.getElementById("customCheck1"),
+    emailInput = document.getElementById("email");
+
+if (localStorage.checkbox && localStorage.checkbox !== "") {
+  rmCheck.setAttribute("checked", "checked");
+  emailInput.value = localStorage.username;
+} else {
+  rmCheck.removeAttribute("checked");
+  emailInput.value = "";
+}
+
+function lsRememberMe() {
+  if (rmCheck.checked && emailInput.value !== "") {
+    localStorage.username = emailInput.value;
+    localStorage.checkbox = rmCheck.value;
+  } else {
+    localStorage.username = "";
+    localStorage.checkbox = "";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
